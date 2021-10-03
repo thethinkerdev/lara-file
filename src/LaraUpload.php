6d9c523e494a4c2aaec707ec3e4b7958c6055fa9
samankhdev <samkh.dev@yahoo.com>
2021-10-04 00:43:28 +0330

    add more feature to fix issues multiple uploading

--- src/LaraUpload.php
+++ src/LaraUpload.php
@@ -2,23 +2,48 @@
 
 namespace Samkhdev\LaraFile;
 
+use Illuminate\Support\Facades\Config;
+use Ramsey\Uuid\Uuid;
+
 class LaraUpload
 {
-    private $data;
+    private $data = [];
     private $names = [];
-    public function __construct(array $data)
+    public $FileName = 'file';
+    public function __construct($data)
     {
-        $this->data = $data;
+        array_push($this->data, ...$data);
     }
     public function send(): array
     {
         foreach ($this->data as $FileInfo) {
-            $name = config('lara-file.get-random-code') . '.' . $FileInfo['file']->getClientOriginalExtension();
-            $disk = $FileInfo['disk'] ?? config("lara-file.default-disk");
-            $dir = $FileInfo['dir'] ?? config("lara-file.default-dir");
+            $name = $this->hashName($FileInfo['file']);
+
+            $disk = $this->getDisk($FileInfo['disk']);
+            $dir = $this->getDir($FileInfo['dir']);
+
             $FileInfo['file']->storeAs($dir, $name, $disk);
             $this->names[$dir] = $name;
         }
         return $this->names;
+    }
+    /* Get Random Code for get unqiue name */
+    private function getRandomCode(): string
+    {
+        return Uuid::uuid4()->toString();
+    }
+
+    private function hashName($file): string
+    {
+        return $this->getRandomCode() . '.' . $file->getClientOriginalExtension();
+    }
+
+    private function getDisk($file): string
+    {
+        return $file ?? config("lara-file.default-disk");
+    }
+    private function getDir($file): string
+    {
+        return $file ?? config("lara-file.default-dir");
     }
 }
--- src/config/laraFile.php
+++ src/config/laraFile.php
@@ -1,9 +1,6 @@
 <?php
 
-use Ramsey\Uuid\Uuid;
-
 return [
     "default-disk" => "public",
-    "default-dir" => "lara-file",
-    "get-random-code" => Uuid::uuid4()->toString()
+    "default-dir" => "lara-file"
 ];
